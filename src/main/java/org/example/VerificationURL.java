25a81f3bdf30201618be73e8ad57ffc2a44b7dda
Iryna Tiutiun <tiutiuniryna@gmail.com>
2024-02-24 16:39:36 +0100

    removed commented code

--- src/main/java/org/example/VerificationURL.java
+++ src/main/java/org/example/VerificationURL.java
@@ -5,7 +5,6 @@ import java.util.regex.Pattern;
 
 public class VerificationURL {
     public static boolean checkURL(String url) {
-//        Pattern pattern = Pattern.compile("^(https?://)(www.)?[a-z{3,9}]\\.[a-z{2,6}](.[a-z]{2,3})");
         Pattern pattern = Pattern.compile("^(https?://)(www.)?[a-z]{3,9}\\.[a-z]{2,6}(.[a-z]{2,3})?");
         Matcher matcher = pattern.matcher(url);
         return matcher.matches();
